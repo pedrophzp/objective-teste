44f4c02aefb01518609e2fae2bcf576de0429522
Pedro Pupin <pedro.phzp@hotmail.com>
2021-03-13 16:30:17 -0300

    update text component
    add title e subtitle

--- src/components/text/index.js
+++ src/components/text/index.js
@@ -1,15 +1,30 @@
-import React from "react"
-import styles from "./index.module.scss"
+import React from "react";
+import styles from "./index.module.scss";
 
-const Text = ({style, children, className}) => {
-  return (
-    <span
-      className={className ? styles[className] : ""}
-      style={{...style }}
-    >
-      {children}
-    </span>
-  );
-}
+const Text = ({ style, children, className, type }) => {
+  switch (type) {
+    case "title":
+      return (
+        <h1 className={`${styles.title} ${className ? styles[className] : ""}`} style={{ ...style }}>
+          {children}
+        </h1>
+      );
+    case "subtitle":
+      return (
+        <h2 className={`${styles.subtitle} ${className ? styles[className] : ""}`} style={{ ...style }}>
+          {children}
+        </h2>
+      );
+    default:
+      return (
+        <span
+          className={className ? styles[className] : ""}
+          style={{ ...style }}
+        >
+          {children}
+        </span>
+      );
+  }
+};
 
-export default Text
+export default Text;
