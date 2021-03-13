3efa646e5b65ec1b9eb402303378baaefc556f2d
Pedro Pupin <pedro.phzp@hotmail.com>
2021-03-12 23:09:12 -0300

    add react-router
    create history inside utils
    create detail page
    make routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Switch } from 'react-router';
+import history from './utils/history';
+
+//Pages
+import Home from './pages/home';
+import Detail from './pages/detail';
+
 import './index.css';
-import Home from './pages/home'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
+  <Router history={history}>
+    <Switch>
+      <Route path="*" component={Home}/>
+      <Route path="/detail/:id" component={Detail}/>
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Home = (props) => {
   return (
-    <h1>Hello</h1>
+    <h1>Home</h1>
   );
 }
 
