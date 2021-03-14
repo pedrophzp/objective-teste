a1750572778d1eaa5fda8e247a5806f0de5474aa
Pedro Pupin <pedro.phzp@hotmail.com>
2021-03-14 02:00:36 -0300

    add loader component

---
+++ src/components/loader/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Loading from "../../images/loading.svg"
+
+import styles from "./index.module.scss";
+
+const Loader = () => {
+  return (
+      <img alt={"loading"} className={styles.loading} src={Loading}/>
+  );
+};
+
+export default Loader;
