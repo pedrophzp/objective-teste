8e82878cb7fb9d3c5241b80040c5e50c0ac1190d
Pedro Pupin <pedro.phzp@hotmail.com>
2021-03-14 03:12:09 -0300

    update Card
    remove tag a and add tag Link to better use inside react-router

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Text from "../text";
 
 import styles from "./index.module.scss";
 
 const Card = ({ id, picture, name, series, events }) => {
   return (
-    <a href={`/detail/${id}`} className={styles.container}>
+    <Link to={`/detail/${id}`} className={styles.container}>
       <img alt={`${name}`} className={styles.picture} src={picture} />
       <Text className={"name"}>{name}</Text>
       <div className={styles.containerDescription}>
@@ -22,7 +23,7 @@ const Card = ({ id, picture, name, series, events }) => {
           </Text>
         ))}
       </div>
-    </a>
+    </Link>
   );
 };
 
